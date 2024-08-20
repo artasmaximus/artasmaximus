c0c92d98f82df9e0c4a76c36892ce5728e166ef6
artasmaximus <artas.maximus.a@gmail.com>
2024-08-20 15:24:55 -0400

    spruce up nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <!-- layout for all routes in the page -->
 <!-- all pages contain a navigation header -->
 
-<script>
+<script lang="ts">
 	import Background from '$lib/components/Background.svelte';
+	import { page } from '$app/stores';
+
+	$: currPage = $page.url.pathname.split('/')[1];
+	console.log(currPage);
 </script>
 
 <Background />
@@ -12,12 +16,11 @@
 	</div>
 
 	<div class="navLinks">
-		<a href={'/projects'}>projects</a>
-		<a href={'/illustrations'}>illustrations</a>
-		<a href={'/contact'}>contact</a>
+		<a href={'/projects'} class:active={currPage === 'projects'}>projects</a>
+		<a href={'/illustrations'} class:active={currPage === 'illustrations'}>illustrations</a>
+		<a href={'/contact'} class:active={currPage === 'contact'}>contact</a>
 	</div>
 </nav>
-
 <slot></slot>
 
 <style>
@@ -30,7 +33,7 @@
 
 	.logo a {
 		font-family: 'Playfair Display', serif;
-		font-weight: black;
+		font-weight: 800;
 		font-size: clamp(24px, 5vw, 32px);
 		color: rgb(36, 36, 36);
 		text-decoration: none;
@@ -42,7 +45,7 @@
 
 	.navLinks {
 		font-family: 'Urbanist';
-		font-weight: 100;
+		font-weight: 300;
 		display: flex;
 		gap: 4rem;
 	}
@@ -50,6 +53,18 @@
 	.navLinks a {
 		text-decoration: none;
 		color: #1a1a1a;
+		font-weight: 300;
+		min-width: 100px; /* Adjust this value to match the longest link */
+		display: inline-block; /* Ensure that the min-width applies correctly */
+		text-align: center; /* Optional: to center the text within the block */
+		transition:
+			font-weight 0.07s ease-in,
+			color 0.07s ease-in-out,
+			letter-spacing 0.07s ease-in-out;
+	}
+
+	.navLinks a.active {
+		font-weight: 700;
 	}
 
 	.navLinks a:hover {
